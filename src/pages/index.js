ad5aaaa586e34c8c0e437cf4887168e3c23dd0ce
Joey <jiangjun12321@sina.com>
2018-04-25 00:33:33 +0800

    build without js.map

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,81 +1,160 @@
-  
-
+    
 import React, { Component } from 'react';
 import withRoot from '../withRoot';    
 import PreviewRoot from '../components/preview/root'
 import PreviewVerticalLayout from '../components/preview/verticalLayout'
 import PreviewVerticalGrid from '../components/preview/verticalGrid'
-import PreviewTextArea from '../components/preview/textArea'
 import PreviewImageArea from '../components/preview/imageArea'
+import PreviewTextArea from '../components/preview/textArea'
     
 class Index extends Component {
   render() {
     return (
     React.createElement(
         PreviewRoot,
-        {"selfkey":"Root_a9c05f404801d35b55a7535ea63b6b5e","parentkey":"root"},
+        {"selfkey":"Root_fcef427b70ee741633101ad0ce93bebd","parentkey":"root"},
 React.createElement(
         PreviewVerticalLayout,
-        {"backgroundInfo":{"background":"#81D4FA","backgroundType":"pureColor","imageInfo":{"width":640,"height":426},"fillType":"stretch","enableParallex":true},"flex":[4,4,4],"id":"VerticalLayout_ab7e4f9b060fd57b7c0966e1ca19d4a8","selfkey":"VerticalLayout_14ce458587c967505dda3deb8533ad21","parentkey":"Root_a9c05f404801d35b55a7535ea63b6b5e"},
+        {"backgroundInfo":{"background":"white","backgroundType":"pureColor","imageInfo":{},"fillType":null,"enableParallex":null},"id":"VerticalLayout_4f669419d2b07e3e2484f75c5c03c6de","flex":[12],"selfkey":"VerticalLayout_d936ce438a4be291c9479a5bcb1d3bf7","parentkey":"Root_fcef427b70ee741633101ad0ce93bebd"},
 React.createElement(
         PreviewVerticalGrid,
-        {"selfkey":"VerticalGrid_0a99d05d54247a036f9395e9fc2535ac","parentkey":"VerticalLayout_14ce458587c967505dda3deb8533ad21"},
+        {"selfkey":"VerticalGrid_ad3c504b03d9958d754a4b45178908aa","parentkey":"VerticalLayout_d936ce438a4be291c9479a5bcb1d3bf7"},
 React.createElement(
-        PreviewTextArea,
-        {"deltaDeltaValue":[{"insert":"这个图片倒了","attributes":{"font":"serif"}},{"insert":"\n","attributes":{"align":"center","header":1}}],"readOnly":false,"selfkey":"TextArea_c2c90dc5299754d2676b120b165c91d8","parentkey":"VerticalGrid_0a99d05d54247a036f9395e9fc2535ac"}
+        PreviewImageArea,
+        {"alt":"initial","src":"http://blog-src.b0.upaiyun.com/taohe/dev/editPage/administrator/1/temporary/layout/8f9130e0761678a017743be3ec38231c","selfkey":"ImageArea_50ca05232c56c20881588ae310202aee","parentkey":"VerticalGrid_ad3c504b03d9958d754a4b45178908aa"}
     ),
 React.createElement(
         PreviewImageArea,
-        {"src":"http://blog-src.b0.upaiyun.com/taohe/dev/editPage/administrator/1/layout/8/9731723d89e723d93ccce2df3bafac9f","selfkey":"ImageArea_83cf1b8fd2a1ff3f91e96cc5c07766e2","parentkey":"VerticalGrid_0a99d05d54247a036f9395e9fc2535ac"}
-    )
-    ),
-React.createElement(
-        PreviewVerticalGrid,
-        {"selfkey":"VerticalGrid_d5eb6350a4453b6316e2b3f468576cb2","parentkey":"VerticalLayout_14ce458587c967505dda3deb8533ad21"},
-React.createElement(
-        PreviewImageArea,
-        {"src":"http://blog-src.b0.upaiyun.com/taohe/dev/editPage/administrator/1/layout/8/df474509fbd8cfd46fd29e6041cf4824","selfkey":"ImageArea_996103a3c297d4f8398e56f40a4b55f6","parentkey":"VerticalGrid_d5eb6350a4453b6316e2b3f468576cb2"}
+        {"alt":"initial","src":"http://blog-src.b0.upaiyun.com/taohe/dev/editPage/administrator/1/temporary/layout/18c703d2d023e8fd0d7cf07bed72c2f1","selfkey":"ImageArea_33288f273fb5b2b83a5998f5f8722908","parentkey":"VerticalGrid_ad3c504b03d9958d754a4b45178908aa"}
     ),
 React.createElement(
         PreviewTextArea,
-        {"deltaDeltaValue":[{"insert":"骚到","attributes":{"font":"serif"}},{"insert":"\n","attributes":{"align":"center","header":1}}],"readOnly":false,"selfkey":"TextArea_c5e21360e24548ad475e054252317778","parentkey":"VerticalGrid_d5eb6350a4453b6316e2b3f468576cb2"}
-    ),
-React.createElement(
-        PreviewImageArea,
-        {"alt":"initial","src":"/images/IMG_7881.jpg","selfkey":"ImageArea_934cd8aa09163a81d3a1e0d334242a9b","parentkey":"VerticalGrid_d5eb6350a4453b6316e2b3f468576cb2"}
-    )
-    ),
-React.createElement(
-        PreviewVerticalGrid,
-        {"selfkey":"VerticalGrid_3db868aa3c781eec0041b5559b0c6089","parentkey":"VerticalLayout_14ce458587c967505dda3deb8533ad21"},
-React.createElement(
-        PreviewTextArea,
-        {"deltaDeltaValue":[{"insert":"右护法"},{"insert":"\n","attributes":{"align":"center","header":1}}],"readOnly":false,"selfkey":"TextArea_8e1f1667d829c10388574ecf176349e9","parentkey":"VerticalGrid_3db868aa3c781eec0041b5559b0c6089"}
-    ),
-React.createElement(
-        PreviewImageArea,
-        {"alt":"initial","src":"/images/IMG_7881.jpg","selfkey":"ImageArea_7a107949a4e88407652e1bc699fef15b","parentkey":"VerticalGrid_3db868aa3c781eec0041b5559b0c6089"}
+        {"deltaDeltaValue":[{"insert":"Winery and Products Introduction","attributes":{"size":"large","bold":true}},{"insert":" ","attributes":{"bold":true}},{"insert":"\n","attributes":{"align":"center","header":1}}],"readOnly":false,"selfkey":"TextArea_24a78165fe68893e27b053b049f442ee","parentkey":"VerticalGrid_ad3c504b03d9958d754a4b45178908aa"}
     )
     )
     ),
 React.createElement(
         PreviewVerticalLayout,
-        {"backgroundInfo":{"background":"#b1d3db","backgroundType":"pureColor","imageInfo":{},"fillType":null,"enableParallex":null},"id":"VerticalLayout_bdb1145fcb5cb642eb9ec4bfe39fda31","selfkey":"VerticalLayout_30828244b64141b3225290fa29dce288","parentkey":"Root_a9c05f404801d35b55a7535ea63b6b5e"},
+        {"backgroundInfo":{"background":"#020A0D","backgroundType":"pureColor","imageInfo":{},"fillType":null,"enableParallex":null},"id":"VerticalLayout_a999e7831d1d4c75c40077cdc7b005c1","flex":[3,6,3],"selfkey":"VerticalLayout_555aba23c3aa7aa5a02781b8e8eaa257","parentkey":"Root_fcef427b70ee741633101ad0ce93bebd"},
 React.createElement(
         PreviewVerticalGrid,
-        {"selfkey":"VerticalGrid_ba55d843894913285bbb37e392f9cf55","parentkey":"VerticalLayout_30828244b64141b3225290fa29dce288"},
+        {"selfkey":"VerticalGrid_39cd32c36d6aee7f0c66c12882bde671","parentkey":"VerticalLayout_555aba23c3aa7aa5a02781b8e8eaa257"},
 React.createElement(
-        PreviewTextArea,
-        {"deltaDeltaValue":[{"insert":"dgdfsgfsdf","attributes":{"font":"serif"}},{"insert":"\n","attributes":{"align":"center","header":1}}],"readOnly":false,"selfkey":"TextArea_fffcf19c45a8a7d5028ee457743e86e9","parentkey":"VerticalGrid_ba55d843894913285bbb37e392f9cf55"}
-    ),
-React.createElement(
-        PreviewTextArea,
-        {"deltaDeltaValue":[{"insert":"在此输入gtgtg","attributes":{"font":"serif"}},{"insert":"\n","attributes":{"align":"center","header":1}}],"readOnly":false,"selfkey":"TextArea_c878238ef30d6eb515dde7723e2e1337","parentkey":"VerticalGrid_ba55d843894913285bbb37e392f9cf55"}
+        PreviewImageArea,
+        {"alt":"initial","src":"http://blog-src.b0.upaiyun.com/taohe/dev/editPage/administrator/1/template/8/96c982abc59d0cea655c5330deb71dae","selfkey":"ImageArea_650913bf9061a83abf0607e9533343ef","parentkey":"VerticalGrid_39cd32c36d6aee7f0c66c12882bde671"}
     )
     ),
 React.createElement(
         PreviewVerticalGrid,
-        {"selfkey":"VerticalGrid_9dec710f830d7e83066e5502dac65f2c","parentkey":"VerticalLayout_30828244b64141b3225290fa29dce288"}
+        {"selfkey":"VerticalGrid_3ccc187011e45558c1bf788f299180e6","parentkey":"VerticalLayout_555aba23c3aa7aa5a02781b8e8eaa257"},
+React.createElement(
+        PreviewTextArea,
+        {"deltaDeltaValue":[{"insert":"\n","attributes":{"align":"center","header":1}},{"insert":"智利圣何塞酒庄(中国)代表处 ","attributes":{"color":"#ffffff","bold":true}},{"insert":"\n","attributes":{"align":"center","header":1}},{"insert":"暨 ","attributes":{"color":"#ffffff","bold":true}},{"insert":"\n","attributes":{"align":"center","header":1}},{"insert":"圣拉蒙贸易(武汉)有限公司 ","attributes":{"color":"#ffffff","bold":true}},{"insert":"\n","attributes":{"align":"center","header":1}}],"readOnly":false,"selfkey":"TextArea_39589cdeb1d3530f4f256f328dceb183","parentkey":"VerticalGrid_3ccc187011e45558c1bf788f299180e6"}
+    )
+    ),
+React.createElement(
+        PreviewVerticalGrid,
+        {"selfkey":"VerticalGrid_407555961181b0caf0213b7b299ea242","parentkey":"VerticalLayout_555aba23c3aa7aa5a02781b8e8eaa257"},
+React.createElement(
+        PreviewImageArea,
+        {"alt":"initial","src":"http://blog-src.b0.upaiyun.com/taohe/dev/editPage/administrator/1/template/8/cc9c0431bbd65720ef0f2e2da2f4820c","selfkey":"ImageArea_81faab07ea9033cc46b5aa0400e3d66b","parentkey":"VerticalGrid_407555961181b0caf0213b7b299ea242"}
+    )
+    )
+    ),
+React.createElement(
+        PreviewVerticalLayout,
+        {"backgroundInfo":{"background":"white","backgroundType":"pureColor","imageInfo":{},"fillType":null,"enableParallex":null},"id":"VerticalLayout_3d7ff068902299b8c9d8cc70d95695ba","flex":[6,6],"selfkey":"VerticalLayout_2d16288becb0bef46a5f4dc64eff093d","parentkey":"Root_fcef427b70ee741633101ad0ce93bebd"},
+React.createElement(
+        PreviewVerticalGrid,
+        {"selfkey":"VerticalGrid_a78fab803894da5cfc60d0b6f5138f12","parentkey":"VerticalLayout_2d16288becb0bef46a5f4dc64eff093d"},
+React.createElement(
+        PreviewTextArea,
+        {"deltaDeltaValue":[{"insert":"智利葡萄酒的名片","attributes":{"bold":true}},{"insert":"\n","attributes":{"align":"center","header":1}},{"insert":"Chile Wine ","attributes":{"bold":true}},{"insert":"\n","attributes":{"align":"center","header":1}},{"insert":"\n","attributes":{"align":"center"}},{"insert":"圣拉蒙贸易(武汉)有限公司是智利圣何 塞酒庄(San Jose de Apalta)在中国设立的代表 处及全职能市场销售公司，代表酒庄在中国市 场进行圣拉蒙(San Ramon)品牌和圣何塞(San Jose de Apalta)品牌的推广、产品营销和市场开发的 活动。公司坚持做专业的智利葡萄酒运营商， 坚持把智利葡萄酒做精做细，对智利葡萄酒的 特点进行精确的把控，所推广的葡萄酒产品曾 多次在国际品鉴大赛中获得金奖和国际著名酒 评人的高酒评分。公司所属的圣何塞酒庄创立 于1970年，隶属于智利前三大农业集团Agricola Santa Cristina Group，集团拥有400公顷的葡萄种 植园和600公顷的农庄果园，主营智利葡萄酒、 智利车厘子等具有智利特色代表的农产品，地 处智利著名的中央山谷里面的拉佩尔谷(Rapel Valley)的阿帕塔产区(Apalta)，世界90%以上 的佳美娜葡萄出产于此地，该产区以出产智利 的顶级佳美娜葡萄酒而闻名于世。 "},{"insert":"\n","attributes":{"header":2}},{"insert":"\n\n"},{"insert":"\n","attributes":{"align":"center"}}],"readOnly":false,"selfkey":"TextArea_6c5cc8d404ad407873f643d79461c25a","parentkey":"VerticalGrid_a78fab803894da5cfc60d0b6f5138f12"}
+    )
+    ),
+React.createElement(
+        PreviewVerticalGrid,
+        {"selfkey":"VerticalGrid_9742235c59ac491c6a21d600cb4e56ff","parentkey":"VerticalLayout_2d16288becb0bef46a5f4dc64eff093d"},
+React.createElement(
+        PreviewImageArea,
+        {"alt":"initial","src":"http://blog-src.b0.upaiyun.com/taohe/dev/editPage/administrator/1/template/8/1bfc866e43dafb3ebc77ea0c1082fa9c","selfkey":"ImageArea_f61cb9d2a907ac5784bc7b189f11247c","parentkey":"VerticalGrid_9742235c59ac491c6a21d600cb4e56ff"}
+    )
+    )
+    ),
+React.createElement(
+        PreviewVerticalLayout,
+        {"backgroundInfo":{"background":"white","backgroundType":"pureColor","imageInfo":{},"fillType":null,"enableParallex":null},"id":"VerticalLayout_48628806d570d91a1705b4d64eddb33f","flex":[6,6],"selfkey":"VerticalLayout_f8e6530fba43be4fb2b65071a3c8d2c2","parentkey":"Root_fcef427b70ee741633101ad0ce93bebd"},
+React.createElement(
+        PreviewVerticalGrid,
+        {"selfkey":"VerticalGrid_aaf182c57ae6c1a9f61a390b64100de5","parentkey":"VerticalLayout_f8e6530fba43be4fb2b65071a3c8d2c2"},
+React.createElement(
+        PreviewTextArea,
+        {"deltaDeltaValue":[{"insert":"圣何塞酒庄 ","attributes":{"size":"large","bold":true}},{"insert":"\n","attributes":{"header":2}}],"readOnly":false,"selfkey":"TextArea_9b3603c37330f67c7319a8fa606c9906","parentkey":"VerticalGrid_aaf182c57ae6c1a9f61a390b64100de5"}
+    ),
+React.createElement(
+        PreviewImageArea,
+        {"alt":"initial","src":"http://blog-src.b0.upaiyun.com/taohe/dev/editPage/administrator/1/template/8/2c062dacd3b1e7e9effa785298edc857","selfkey":"ImageArea_9af565c1abe6e6c5e90a9196db74bc41","parentkey":"VerticalGrid_aaf182c57ae6c1a9f61a390b64100de5"}
+    )
+    ),
+React.createElement(
+        PreviewVerticalGrid,
+        {"selfkey":"VerticalGrid_c2c1b3623b7b44ecaa8f04ac4e4673bc","parentkey":"VerticalLayout_f8e6530fba43be4fb2b65071a3c8d2c2"},
+React.createElement(
+        PreviewTextArea,
+        {"deltaDeltaValue":[{"insert":"圣何塞阿帕塔 "},{"insert":"\n","attributes":{"align":"center","header":2}},{"insert":"(San Jose de Apalta)"},{"insert":"\n","attributes":{"align":"center","header":2}},{"insert":"\n","attributes":{"align":"center"}},{"insert":"  坐落智利首都圣地亚哥旁140公里处的圣 何塞阿帕塔(San Jose de Apalta)酒庄，创立 于智利葡萄酒业技术快速发展的1970年，成立 初期以家族经营的模式致力于栽种精细的葡萄， 直至1994年，以丰富热情的栽植经验与专业酿 酒知识，以“执着完美”的精神开始生产高品 质且风格独具的葡萄酒。庄园的家族葡萄园位 于智利中部的拉佩尔谷产区(Rapel Valley)， 向来以独特的土壤及气候闻名。承袭家族经营 的方式使园内每一株葡萄树叶皆由人工细心及 完善地照顾，其超过30年树龄的枝藤更蕴育出 多种极具收藏价值的珍酿。总年产量约320万 升，至今已成功行销全球各地，成为智利葡萄 酒外销酒庄的翘楚之一，并赢得顶级葡萄酒的 美誉。  "},{"insert":"\n","attributes":{"header":2}}],"readOnly":false,"selfkey":"TextArea_4ee2ed93549d298f20b8be110e9ba988","parentkey":"VerticalGrid_c2c1b3623b7b44ecaa8f04ac4e4673bc"}
+    )
+    )
+    ),
+React.createElement(
+        PreviewVerticalLayout,
+        {"backgroundInfo":{"background":"url(http://blog-src.b0.upaiyun.com/taohe/dev/editPage/administrator/1/template/8/1e59b831cea08d6cb87f0e331fe2cb61)","backgroundType":"image","imageInfo":{"width":1712,"height":294},"fillType":"stretch","enableParallex":false},"id":"VerticalLayout_cc9cf35ab755bbd1fe6c901143e6b7b6","flex":[12],"selfkey":"VerticalLayout_cc9cf35ab755bbd1fe6c901143e6b7b6","parentkey":"Root_fcef427b70ee741633101ad0ce93bebd"},
+React.createElement(
+        PreviewVerticalGrid,
+        {"selfkey":"VerticalGrid_6d40c1967b6164119894d5c7dc1404f4","parentkey":"VerticalLayout_cc9cf35ab755bbd1fe6c901143e6b7b6"},
+React.createElement(
+        PreviewTextArea,
+        {"deltaDeltaValue":[{"insert":"\n","attributes":{"align":"center","header":1}}],"readOnly":false,"selfkey":"TextArea_3f4b3946016d4e65483144bf6e6ff5e1","parentkey":"VerticalGrid_6d40c1967b6164119894d5c7dc1404f4"}
+    )
+    )
+    ),
+React.createElement(
+        PreviewVerticalLayout,
+        {"backgroundInfo":{"background":"white","backgroundType":"pureColor","imageInfo":{},"fillType":null,"enableParallex":null},"id":"VerticalLayout_a98ac090c039c9fc45e4e393460e3c92","flex":[12],"selfkey":"VerticalLayout_a98ac090c039c9fc45e4e393460e3c92","parentkey":"Root_fcef427b70ee741633101ad0ce93bebd"},
+React.createElement(
+        PreviewVerticalGrid,
+        {"selfkey":"VerticalGrid_158f2cffaf3928273f877505d6d30bff","parentkey":"VerticalLayout_a98ac090c039c9fc45e4e393460e3c92"},
+React.createElement(
+        PreviewTextArea,
+        {"deltaDeltaValue":[{"insert":"圣何塞阿帕塔酒庄葡萄酒生产基地 ","attributes":{"bold":true}},{"insert":"\n","attributes":{"align":"center","header":1}},{"insert":"The Wine Production Factory ","attributes":{"size":"large","bold":true}},{"insert":"\n","attributes":{"align":"center","header":3}}],"readOnly":false,"selfkey":"TextArea_c67d5002aa95cef6b50db3f91f1b2625","parentkey":"VerticalGrid_158f2cffaf3928273f877505d6d30bff"}
+    )
+    )
+    ),
+React.createElement(
+        PreviewVerticalLayout,
+        {"backgroundInfo":{"background":"#b1d3db","backgroundType":"pureColor","imageInfo":{},"fillType":null,"enableParallex":null},"id":"VerticalLayout_90ae6bebc1338ee7d8050079710b072a","flex":[4,4,4],"selfkey":"VerticalLayout_90ae6bebc1338ee7d8050079710b072a","parentkey":"Root_fcef427b70ee741633101ad0ce93bebd"},
+React.createElement(
+        PreviewVerticalGrid,
+        {"selfkey":"VerticalGrid_7117084d8d10dccb29d908edbbfc23e1","parentkey":"VerticalLayout_90ae6bebc1338ee7d8050079710b072a"},
+React.createElement(
+        PreviewImageArea,
+        {"alt":"initial","src":"http://blog-src.b0.upaiyun.com/taohe/dev/editPage/administrator/1/template/8/ae3a74ad2d8d89ed4bcb04a828edfbef","selfkey":"ImageArea_68e0e6e77305ae152873fb0b78ad5341","parentkey":"VerticalGrid_7117084d8d10dccb29d908edbbfc23e1"}
+    )
+    ),
+React.createElement(
+        PreviewVerticalGrid,
+        {"selfkey":"VerticalGrid_87f1f45598d4f38934c5cfefaa00a4b9","parentkey":"VerticalLayout_90ae6bebc1338ee7d8050079710b072a"},
+React.createElement(
+        PreviewImageArea,
+        {"alt":"initial","src":"http://blog-src.b0.upaiyun.com/taohe/dev/editPage/administrator/1/template/8/765e760ab2fc247682cb1e9bdc4d534a","selfkey":"ImageArea_943cfeae2383dfd84941080a49b075a6","parentkey":"VerticalGrid_87f1f45598d4f38934c5cfefaa00a4b9"}
+    )
+    ),
+React.createElement(
+        PreviewVerticalGrid,
+        {"selfkey":"VerticalGrid_da7fb7b21eb64cb157c72458f45bf210","parentkey":"VerticalLayout_90ae6bebc1338ee7d8050079710b072a"},
+React.createElement(
+        PreviewImageArea,
+        {"alt":"initial","src":"http://blog-src.b0.upaiyun.com/taohe/dev/editPage/administrator/1/template/8/cfd77b61722815b42c70c5998888d61c","selfkey":"ImageArea_b6b6ab6321294b5e95dff20628c76986","parentkey":"VerticalGrid_da7fb7b21eb64cb157c72458f45bf210"}
+    )
     )
     )
     )
@@ -84,5 +163,5 @@ React.createElement(
 }
 
 export default withRoot(Index);    
-
-
+    
+  
